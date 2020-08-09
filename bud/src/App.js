f78fc52cf8baaf62b3c3b9329d59081a83310eac
carlsachs <sachscarl@gmail.com>
2020-08-09 00:38:46 -0400

    initial push

---
+++ bud/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+function App() {
+  return (
+    <Container>
+      <Row className="topRow">
+
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
